8e3e637dd345f3b13ffe88a1f6cf48bb221f6bf5
jackzhao-mj <jackzhao.mj@gmail.com>
2016-10-05 15:15:42 -0700

    licence

--- simulation/simulation_centralized.py
+++ simulation/simulation_centralized.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2013 The Regents of The University California
+# Copyright 2016 The Regents of The University California
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
